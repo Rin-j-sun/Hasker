bd2fd94532cc4d456143237f7b99fbc65a37d964
gr421_sosdm <sabrina.sotnikova@gmail.com>
2023-11-16 15:51:06 +0700

    Добавление компонентов. Пока без фатальных ошибок

---
+++ src/components/Articleltems.vue
@@ -0,0 +1,24 @@
+<template>
+    <ListItems :items="items">
+        <template v-slot:default="props">
+            <ArticleItem :item="props.item"></ArticleItem>
+        </template>
+        <template v-slot:empty>
+            Статьи еще пишутся :)
+        </template>
+    </ListItems>
+</template>
+
+<script>
+import ListItems from '@/components/ListItems.vue'
+import ArticleItem from '@/components/ArticleItem.vue'
+
+export default {
+    name: 'ArticleItems',
+    components: {
+        ArticleItem,
+        ListItems,
+    },
+    extends: ListItems,
+}
+</script>
--- src/pages/Category.vue
+++ src/pages/Category.vue
@@ -1,13 +1,46 @@
 <template>
-
+    <div>
+        <div v-if="category">
+            <h1>
+                {{ category.title }}
+            </h1>
+            <ArticleItems :items="articles"></ArticleItems>
+        </div>
+        <div v-else>
+            Категория не найдена
+        </div>
+    </div>
 </template>
 
 <script>
+import ArticleItems from '@/components/ArticleItems.vue'
+import {
+    mapActions,
+} from 'vuex'
+
 export default {
-    name: "Category"
+    name: 'Category',
+    components: {
+        ArticleItems,
+    },
+    computed: {
+        categoryId() {
+            return this.$route.params['category_id'] || null;
+        },
+        category() {
+            return this.$store.state.blog.activeCategory;
+        },
+        articles() {
+            return this.$store.getters.activeCategoryArticles;
+        },
+    },
+    methods: {
+        ...mapActions([
+            'loadActiveCategory',
+        ]),
+    },
+    mounted() {
+        this.loadActiveCategory(this.categoryId);
+    },
 }
 </script>
-
-<style scoped>
-
-</style>
